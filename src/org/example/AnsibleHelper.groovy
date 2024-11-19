5a3771230b7e891c3b4c7bb4a658d9a3effc2ccc
Makara <148768781+Makara1122@users.noreply.github.com>
2024-11-20 00:22:13 +0700

    Update AnsibleHelper.groovy

--- src/org/example/AnsibleHelper.groovy
+++ src/org/example/AnsibleHelper.groovy
@@ -1,15 +1,5 @@
-package org.example
-
-class AnsibleHelper {
-    def steps
-
-    AnsibleHelper(steps) {
-        this.steps = steps
-    }
-
-    def runPlaybook(String playbookPath, String inventoryPath, String ansibleArgs = '') {
-        steps.sh """
-        ansible-playbook -i ${inventoryPath} ${playbookPath} ${ansibleArgs}
-        """
-    }
+def runPlaybook(steps, String playbookPath, String inventoryPath, String ansibleArgs = '') {
+    steps.sh """
+    ansible-playbook -i ${inventoryPath} ${playbookPath} ${ansibleArgs}
+    """
 }
